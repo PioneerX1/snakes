7f69b711e586020485c237ab0dde9bf47ed8847a
Laura Hamilton and Mick Sexton <mick@>
2017-05-30 16:04:01 -0700

    Initial webiste setup

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $(".clickable1").click(function() {
+    $("#bite-showing").toggle();
+    $("#bite-hidden").toggle();
+  });
+  $(".clickable2").click(function() {
+    $("#anaconda-showing").toggle();
+    $("#anaconda-hidden").toggle();
+  });
+  $(".clickable3").click(function() {
+  $("#cow-showing").toggle();
+  $("#cow-hidden").toggle();
+});
+});
